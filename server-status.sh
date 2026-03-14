9eb2480eb8588507717c96e10f966d16221fd940
Akash <akashbabuchovvatukunnel@gmail.com>
2026-03-14 20:49:13 +0530

    Added Cpu,memory and disk usage stat to server monitoring script

--- server-status.sh
+++ server-status.sh
@@ -3,14 +3,35 @@
 echo "####==== SERVER PERFORMANCE STATUS ====####"
 echo ""
 
-echo "====____ CPU USAGE ____===="
-# $8 is the idle CPU percentage, so we subtract it from 100 to get the actual CPU usage.
-top -bn1 | grep "Cpu(s):" | awk'{print "Cpu Usage:" 100 - $8 "%" }
+echo "==== CPU USAGE ===="
+
+# $8 = idle CPU → subtract from 100 to get usage
+top -bn1 | grep "Cpu(s)" | awk '{printf "CPU Usage: %.2f%%\n", 100-$8}'
+
 echo ""
 
-echo "====____ MEMORY USAGE ____===="
-# The free command with the -m option displays memory usage in megabytes. We use awk to extract the total and free memory values from the second line of the output (NR==2) and calculate the percentage of memory usage.
-free -m | awk 'NR==2{"Total Memory: %s \nFree Memory: %s \n Usage: %.2f%%\n",$2,$3,$4,$3*100/$2}'
+echo "####==== MEMORY USAGE ====####"
+
+# NR==2 selects memory row → $2 total, $3 used, $4 free
+free -m | awk 'NR==2 {printf "Total Memory: %s MB\nUsed Memory: %s MB\nFree Memory: %s MB\nUsage: %.2f%%\n",$2,$3,$4,$3*100/$2}'
+
 echo ""
 
+echo "####==== DISK USAGE ====####"
 
+# Extract root disk usage and format Total, Used, Free and Usage from df output
+df -h / | awk 'NR==2{printf "Total: %s\nUsed: %s\nFree: %s\nUsage: %s\n",$2,$3,$4,$5}'
+
+echo ""
+
+echo "####==== Top 5 CPU Processes ====####"
+
+# List all processes sorted by highest CPU usage and display the top 5
+ps aux --sort=-%cpu | head -6
+
+echo ""
+
+echo "####==== Top 5 Memory Processes ====####"
+
+# List all processes sorted by highest Memory usage and display the top 5
+ps aux --sort=-%mem | head -6
